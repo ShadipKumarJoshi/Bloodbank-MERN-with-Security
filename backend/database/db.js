516e155b636fee01ed9e6d3b0bbf28652c5caf4a
Shadip Kumar Joshi <220049@softwarica.edu.np>
2025-01-28 04:20:54 +0545

    db log

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,14 +1,28 @@
 
 const mongoose = require('mongoose');
+const winston = require('winston');
+
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'application.log' })
+    ]
+});
 
 
 const connectDB = () => {
     mongoose.connect(process.env.DB_URL)
         .then(() => {
-            info("Connected to Database");
+            logger.info("Connected to Database");
         })
         .catch(error => {
-            error("Database connection error", { error: error.message });
+            logger.error("Database connection error", { error: error.message });
         });
 }
 
